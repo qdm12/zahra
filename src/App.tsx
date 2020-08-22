20358a8e362758cb60a2fa4956185c93b53ea01f
Quentin McGaw <quentin.mcgaw@gmail.com>
2020-08-22 12:27:22 -0400

    Wire up App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,95 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import WelcomeScreen from "components/WelcomeScreen";
+import IconZahraGreen from "components/IconZahraGreen";
+import FoodMenu from "components/FoodMenu";
+import { fetchYML, parseYMLData } from "logic/yml";
+import { detectBrowserLanguage, getAllTranslations } from "logic/translations";
+import { IntlProvider } from "react-intl";
+import { Menu } from "logic/models";
+import ErrorSnackbar from "components/ErrorSnackbar";
+import Booking from "components/Booking";
+import GoogleMaps from "components/GoogleMaps";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
     width: "100%",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "red",
+    minHeight: "100vh",
+    backgroundColor: "#adb5aa",
+    paddingBottom: theme.spacing(3),
   },
-});
+  icon: {
+    display: "flex",
+    width: "100%",
+    justifyContent: "center",
+    paddingTop: 30,
+    fontSize: "20em",
+    marginBottom: theme.spacing(6),
+    transition: "all 1s ease",
+    contrast: 1,
+    "&:hover": {
+      filter: "contrast(1.4)",
+    },
+  },
+  menu: {},
+  booking: {
+    marginTop: theme.spacing(5),
+  },
+  maps: {
+    marginTop: theme.spacing(5),
+  },
+}));
+
+const language = detectBrowserLanguage();
+const rootURL = window.location.href;
 
 function App(): JSX.Element {
   const classes = useStyles();
-  return <div className={classes.root}>Text</div>;
+  const errorRef = useRef("");
+  const [showError, setShowError] = useState(false);
+  // TODO initialize here as well
+  const [translations, setTranslations] = useState<Record<string, Record<string, string>>>({});
+  const [menu, setMenu] = useState<Menu>({ sections: [] });
+
+  // Helper functions
+  const setError = (s: string): void => {
+    errorRef.current = s;
+    setShowError(true);
+  };
+  const clearError = (): void => {
+    errorRef.current = "";
+    setShowError(false);
+  };
+
+  useEffect(() => {
+    const asyncFunc = async (): Promise<void> => {
+      try {
+        const data = parseYMLData(await fetchYML(rootURL));
+        setTranslations(getAllTranslations(data.translations));
+        setMenu(data.menu);
+      } catch (e) {
+        setError(e.toString());
+      }
+    };
+    asyncFunc();
+  }, []);
+
+  if (JSON.stringify(translations) === "{}") {
+    return <WelcomeScreen />;
+  }
+
+  return (
+    <IntlProvider locale={language} messages={translations[language]} defaultLocale="ro">
+      <div className={classes.root}>
+        <WelcomeScreen />
+        <IconZahraGreen className={classes.icon} />
+        <FoodMenu menu={menu} className={classes.menu} />
+        <Booking setError={setError} clearError={clearError} className={classes.booking} />
+        <GoogleMaps className={classes.maps} />
+        <ErrorSnackbar open={showError} message={errorRef.current} onClose={clearError} />
+      </div>
+    </IntlProvider>
+  );
 }
 
 export default App;
