69737fe0376ae57cca24acbf2a04842925251da0
Quentin McGaw <quentin.mcgaw@gmail.com>
2020-10-25 20:23:20 -0400

    Moare keywords

--- src/components/MetaSEO.tsx
+++ src/components/MetaSEO.tsx
@@ -5,7 +5,10 @@ function MetaSEO(): JSX.Element {
   return (
     <Helmet>
       <meta name="description" content="Zahra restaurant" />
-      <meta name="keywords" content="Zahra restaurant, Arabic restaurant, food, restaurant, Bucharest, Pipera" />
+      <meta
+        name="keywords"
+        content="Zahra restaurant, Arabic restaurant, food, restaurant, Bucharest, Pipera, Middle eastern, lebanese cuisine, food delivery Pipera, takeaway Pipera"
+      />
     </Helmet>
   );
 }
